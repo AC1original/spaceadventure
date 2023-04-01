99d76db9d9e16238cdec80827be74aed947c89ec
uwu <ac1original.lol@gmail.com>
2023-04-01 11:11:46 +0200

    changes lol

--- spaceadventure/src/utility/Utils.java
+++
@@ -1,7 +0,0 @@
-package utility;
-
-public class Utils {
-    public static int MAX_PLAYER_HEALTH = 3;
-    public static int MAX_PLAYER_AMMO = 7;
-    public static int MAIN_MENU = 0, GAME = 1, PAUSE_MENU = 2, GAME_OVER = 3, SHOP = 4;
-}
